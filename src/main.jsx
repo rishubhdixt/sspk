40ac17afcab3a7e32714da5fac59b169cf78fa6f
rishubh dixit <rishubhdixit384@gmail.com>
2025-04-22 23:31:44 +0530

    stable version of vite and react installed

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { ParallaxProvider } from 'react-scroll-parallax';
+
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
@@ -9,9 +9,9 @@ import { BrowserRouter } from 'react-router-dom'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-    <ParallaxProvider>
+    
       <App />
-      </ParallaxProvider>
+    
     </BrowserRouter>
   </StrictMode>,
 );
